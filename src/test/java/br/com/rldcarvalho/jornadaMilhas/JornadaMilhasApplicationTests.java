c8c426d1171645a7cf8bda8196cb4fd5117d89ca
rldcarvalho <rldcarvalhoo@gmail.com>
2023-08-17 21:02:39 -0300

    Add base project

---
+++ src/test/java/br/com/rldcarvalho/jornadaMilhas/JornadaMilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rldcarvalho.jornadaMilhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaMilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
