0c167eea9997a1a85faa827a7d5f250f9c894115
rldcarvalho <rldcarvalhoo@gmail.com>
2023-08-24 22:25:06 -0300

    Add post Testimonial

--- src/main/java/br/com/rldcarvalho/jornadaMilhas/controller/TestimonialController.java
+++ src/main/java/br/com/rldcarvalho/jornadaMilhas/controller/TestimonialController.java
@@ -1,9 +1,17 @@
 package br.com.rldcarvalho.jornadaMilhas.controller;
 
+import br.com.rldcarvalho.jornadaMilhas.model.testmonial.Testimonial;
+import br.com.rldcarvalho.jornadaMilhas.model.testmonial.TestimonialCreateData;
+import br.com.rldcarvalho.jornadaMilhas.model.testmonial.TestimonialDetailData;
 import br.com.rldcarvalho.jornadaMilhas.repository.TestimonialRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/depoimentos")
@@ -11,4 +19,19 @@ public class TestimonialController {
 
     @Autowired
     private TestimonialRepository repository;
+
+    @PostMapping
+    @Transactional
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity postTestimonial(@RequestBody TestimonialCreateData testimonialCreateData, UriComponentsBuilder uriBuilder){
+
+        Testimonial testimonial = repository.save(new Testimonial(testimonialCreateData));
+
+        URI uri = uriBuilder.path("/depoimentos/{id}").buildAndExpand(testimonial.getId()).toUri();
+
+        TestimonialDetailData testimonialDetailData = new TestimonialDetailData(testimonial);
+
+        return ResponseEntity.created(uri).body(testimonialDetailData);
+
+    }
 }
