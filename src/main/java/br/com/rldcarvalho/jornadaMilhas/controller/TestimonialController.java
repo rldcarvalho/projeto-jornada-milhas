5e3ab11d773e0bdf5a51c6211e80a0529aa4ec6a
rldcarvalho <rldcarvalhoo@gmail.com>
2023-08-29 15:53:27 -0300

    Add get random Testimonial

--- src/main/java/br/com/rldcarvalho/jornadaMilhas/controller/TestimonialController.java
+++ src/main/java/br/com/rldcarvalho/jornadaMilhas/controller/TestimonialController.java
@@ -6,6 +6,7 @@ import br.com.rldcarvalho.jornadaMilhas.model.testmonial.TestimonialDetailData;
 import br.com.rldcarvalho.jornadaMilhas.repository.TestimonialRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,16 +18,16 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/depoimentos")
+@RequestMapping
 public class TestimonialController {
 
     @Autowired
     private TestimonialRepository repository;
 
-    @PostMapping
+    @PostMapping(value = "/depoimentos")
     @Transactional
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity postTestimonial(@RequestBody TestimonialSimpleData testimonialSimpleData, UriComponentsBuilder uriBuilder){
+    public ResponseEntity postTestimonial(@RequestBody @Valid TestimonialSimpleData testimonialSimpleData, UriComponentsBuilder uriBuilder){
 
         Testimonial testimonial = repository.save(new Testimonial(testimonialSimpleData));
 
@@ -38,14 +39,19 @@ public class TestimonialController {
 
     }
 
-    @GetMapping
+    @GetMapping(value = "/depoimentos")
     public ResponseEntity<List<TestimonialDetailData>> getAllTestimonial(){
         return ResponseEntity.ok(repository.findAll().stream().filter(Testimonial::isActive).map(TestimonialDetailData::new).toList());
     }
 
-    @PutMapping(value = "/{id}")
+    @GetMapping(value = "/depoimentos-home")
+    public ResponseEntity<List<TestimonialDetailData>> getRandomTestimonial(){
+        return ResponseEntity.ok(repository.findRandomTestimonials().stream().map(TestimonialDetailData::new).toList());
+    }
+
+    @PutMapping(value = "/depoimentos/{id}")
     @Transactional
-    public ResponseEntity<TestimonialDetailData> updateTestimonial(@PathVariable Long id, @RequestBody TestimonialSimpleData testimonialSimpleData){
+    public ResponseEntity<TestimonialDetailData> updateTestimonial(@PathVariable Long id, @RequestBody @Valid TestimonialSimpleData testimonialSimpleData){
         Optional<Testimonial> data = repository.findById(id);
         if (data.isEmpty()){
             throw new EntityNotFoundException("Testimonial id not found");
@@ -55,7 +61,7 @@ public class TestimonialController {
         return ResponseEntity.ok().body(new TestimonialDetailData(testimonial));
     }
 
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(value = "/depoimentos/{id}")
     @Transactional
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> deleteTestimonial(@PathVariable Long id){
--- src/main/java/br/com/rldcarvalho/jornadaMilhas/repository/TestimonialRepository.java
+++ src/main/java/br/com/rldcarvalho/jornadaMilhas/repository/TestimonialRepository.java
@@ -2,6 +2,12 @@ package br.com.rldcarvalho.jornadaMilhas.repository;
 
 import br.com.rldcarvalho.jornadaMilhas.model.testmonial.Testimonial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TestimonialRepository extends JpaRepository<Testimonial, Long> {
+
+    @Query(value = "SELECT * FROM testimonial WHERE active = true ORDER BY RAND() LIMIT 3", nativeQuery = true)
+    List<Testimonial> findRandomTestimonials();
 }
