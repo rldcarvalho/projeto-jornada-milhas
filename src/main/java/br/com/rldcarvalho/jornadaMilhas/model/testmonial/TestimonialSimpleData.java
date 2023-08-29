c0f0f0746379c36dd828ddec6f962a03bcce69e6
rldcarvalho <rldcarvalhoo@gmail.com>
2023-08-29 15:52:55 -0300

    Add Testimonial simple validator

--- src/main/java/br/com/rldcarvalho/jornadaMilhas/model/testmonial/TestimonialSimpleData.java
+++ src/main/java/br/com/rldcarvalho/jornadaMilhas/model/testmonial/TestimonialSimpleData.java
@@ -1,7 +1,11 @@
 package br.com.rldcarvalho.jornadaMilhas.model.testmonial;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record TestimonialSimpleData(
+        @NotBlank
         String personName,
+        @NotBlank
         String testimonialText,
         String imagePath
 ) {}
