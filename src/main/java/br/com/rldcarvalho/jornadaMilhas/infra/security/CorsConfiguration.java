0ca332519720753f8aca67cc39a1ce5c40fbeb36
rldcarvalho <rldcarvalhoo@gmail.com>
2023-09-06 22:51:25 -0300

    Add Cors Configuration

---
+++ src/main/java/br/com/rldcarvalho/jornadaMilhas/infra/security/CorsConfiguration.java
@@ -0,0 +1,13 @@
+package br.com.rldcarvalho.jornadaMilhas.infra.security;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
